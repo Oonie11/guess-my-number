fca9f27b25aca63261da30ca5b72442e9bd22a8b
Oonie11 <utkarshpatel002@gmail.com>
2022-09-27 21:37:01 -0400

    add you lost game

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ const secretNum = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNum;
 
 //Keeping a score
-let score = Number(document.querySelector(".score").textContent);
-console.log(typeof score);
+let score = 20;
 let highScore = 0;
 
 //declaring a variable for click button
@@ -27,7 +26,6 @@ const clickButton = document.querySelector(".check");
 clickButton.addEventListener("click", () => {
   //declaring a variable to store user input
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   //conditional statement to compare the userInput with Secret number
   if (!guess) {
@@ -35,11 +33,23 @@ clickButton.addEventListener("click", () => {
   } else if (guess === secretNum) {
     document.querySelector(".message").textContent = "🎉 Correct number !";
   } else if (guess > secretNum) {
-    document.querySelector(".message").textContent = "📈 Too high";
-    document.querySelector(".score").textContent = `${(score -= 1)}`;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the Game !";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNum) {
-    document.querySelector(".message").textContent = "📉 Too low";
-    document.querySelector(".score").textContent = `${(score -= 1)}`;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the Game !";
+      document.querySelector(".score").textContent = 0;
+    }
   } else {
     document.querySelector(".message").textContent = "Error Found";
   }
